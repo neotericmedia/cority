c7427729c235bf8658ee89e367bdb5f996714a12
Douglas Garrigan <douglasg@procom.ca>
2019-04-05 14:52:09 -0400

    _

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cority';
+  hidden = false;
+
+  data = [
+    { status: 'new', icon: 'icon', text: 'asdfasdfasdf' },
+    { status: 'not', icon: 'icon', text: 'asdfasdfasdf' },
+    { status: 'not', icon: 'icon', text: 'asdfasdfasdfasdf' }
+  ];
+
+  ngOnInit() {
+    this.hidden = true;
+  }
+
+  collapse() {
+    // hidden ? true : false
+    console.log('collapse');
+  }
+
+  downloadListItem() {
+    console.log('download');
+  }
+
+  deleteListItem() {
+    console.log('delete');
+  }
 }
